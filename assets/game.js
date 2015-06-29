855ce4a45432439688e6b9751c93c23f6a26a20f
cryptolemming <aliayoub22@gmail.com>
2015-06-29 00:10:31 -0400

    ghost counter and semi-functioning AI ghost

--- assets/game.js
+++ assets/game.js
@@ -56,7 +56,12 @@
 	// when a key is pressed
 	window.addEventListener('keydown', function(e) {
 		keysDown[e.keyCode] = true;
-		keysToggle[e.keyCode] = keysToggle[e.keyCode] == true ? false : true;
+		isGhostReady(e, keysToggle, this.ghostcounter1);
+		
+		// prevent ghost toggling memory when ghost counter not counted down
+		function isGhostReady(e, keysToggle, ghostcounter1) {
+			this.keysToggle[e.keyCode] = this.keysToggle[e.keyCode] !== true && ghostcounter1.ghostCounter.count === 'Press "g"' ? true : false;
+		}
 	});
 	
 	// when a key is let go
@@ -83,8 +88,15 @@
 	function Score(x, y, count) {
 		this.x = x;
 		this.y = y;
-		this.count = 0;
+		this.count = count;
 	};
+	
+	// define a ghost counter
+	function GhostCounter(x, y, count) {
+		this.x = x;
+		this.y = y;
+		this.count = count;
+	}
 	
 	// define a game ball
 	function Ball(x, y) {
@@ -95,6 +107,8 @@
 		this.y_speed = 0;
 		this.score1 = 0;
 		this.score2 = 0;
+		this.ghostcounter1 = -5;
+		this.ghostcounter2 = -5;
 	};
 	
 	// define game assets
@@ -105,14 +119,23 @@
 	
 	function Computer() {
 		this.paddle = new Paddle(.01*canvas.width, .4*canvas.height, .01*canvas.width, .2*canvas.height, 'white');
+		this.ghost = new Paddle(.49*canvas.width, .4*canvas.height, .01*canvas.width, .2*canvas.height, 'rgba(255, 255, 255, 0)');
 	};
 	
 	function ScoreOne() {
-		this.score = new Score(.83*canvas.width, .05*canvas.height, 0);
+		this.score = new Score(.78*canvas.width, .05*canvas.height, 0);
 	};
 	
 	function ScoreTwo() {
 		this.score = new Score(.05*canvas.width, .05*canvas.height, 0);
+	};
+	
+	function GhostCounterOne() {
+		this.ghostCounter = new GhostCounter(.85*canvas.width, .05*canvas.height, -5);
+	};
+	
+	function GhostCounterTwo() {
+		this.ghostCounter = new GhostCounter(.12*canvas.width, .05*canvas.height, -5);
 	};
 	
 	// add render method to paddle prototype
@@ -126,6 +149,13 @@
 		ctx.font = '20px Helvetica';
 		ctx.fillStyle = 'rgba(255, 255, 255, .6)';
 		ctx.fillText('Score: ' + this.count, this.x, this.y);
+	};
+	
+	// add render method to ghost countdown property
+	GhostCounter.prototype.render = function() {
+		ctx.font = '20px Helvetica';
+		ctx.fillStyle = 'rgba(255, 255, 255, .6)';
+		ctx.fillText('Ghost: ' + this.count, this.x, this.y);
 	};
 	
 	// add render method to ball prototype
@@ -143,7 +173,8 @@
 	};
 	
 	Computer.prototype.render = function() {
-		this.paddle.render('white');
+		this.paddle.render();
+		this.ghost.render();
 	};
 	
 	ScoreOne.prototype.render = function() {
@@ -153,6 +184,14 @@
 	ScoreTwo.prototype.render = function() {
 		this.score.render();
 	};
+	
+	GhostCounterOne.prototype.render = function() {
+		this.ghostCounter.render();
+	};
+	
+	GhostCounterTwo.prototype.render = function() {
+		this.ghostCounter.render();
+	};
 
 // END - Game Asset Definitions and Rendering
 
@@ -160,29 +199,35 @@
 
 	// game asset updates
 	var update = function() {
-		player.update();
+		player.update(ball);
 		computer.update(ball);
-		scoreone.update(ball);
-		scoretwo.update(ball);
-		ball.update(player.paddle, computer.paddle, player.ghost);
+		score1.update(ball);
+		score2.update(ball);
+		ghostcounter1.update(ball);
+		ghostcounter2.update(ball);
+		ball.update(player.paddle, computer.paddle, player.ghost, computer.ghost);
 	};
 	
-	// player position updating / movement
-	Player.prototype.update = function() {
+	// player and ghost position updating / movement
+	Player.prototype.update = function(ball) {
 		var _this = this;
 		var lowercaseG = keysToggle[71];
 		
+		// switch between player and ghost paddles
 		for(var key in keysDown) {
 			var value = Number(key);
-			if(lowercaseG) {
+			if(lowercaseG && ball.ghostcounter1 === 'Press "g"') {
 				toggleGhost(value);
 			} else {
 				togglePlayer(value);
 			}
 		};
 		
+		// player paddle active and ghost paddle not active
 		function togglePlayer(value) {
-			_this.ghost.color = 'rgba(255, 255, 255, 0)';
+			setTimeout(function() {
+				_this.ghost.color = 'rgba(255, 255, 255, 0)';
+			}, 150);
 			if(value == 38) {
 				// paddle moves by 10% of paddle height
 				_this.paddle.move(0, -(.02*canvas.height));
@@ -193,11 +238,13 @@
 			}
 		};
 		
+		// ghost paddle active and player paddle not active
 		function toggleGhost(value) {
 			_this.ghost.color = 'rgba(255, 255, 255, .4)';
 			moveGhost(value);
 		};
 		
+		// ghost paddle movement
 		function moveGhost(value) {
 			if(value == 38) {
 				_this.ghost.move(0, -(.02*canvas.height));
@@ -211,24 +258,41 @@
 	
 	// AI 
 	Computer.prototype.update = function(ball) {
+		var _this = this;
 		// take the position of ball top
-		var y_pos = ball.y + ball.radius;
+		var y_pos = ball.y + this.radius;
 		// take the difference between the center of the paddle and the ball
 		var diff = -(this.paddle.y + this.paddle.height / 2 - y_pos);
-		// limit AI paddle speed
+
+		// set AI speed
 		if(diff < 0 && diff < -.02*canvas.height) { // max speed up
 			diff = -.02*canvas.height;
 		} else if(diff > 0 && diff > .02*canvas.height) { // max speed down
 			diff = .02*canvas.height;
 		}
-		// move the paddle so its center aligns with the center of the ball
-		this.paddle.move(0, diff);
-		// computer paddle goes off screen to top
-		if(this.paddle.y < 0) {
-			this.paddle.y = 0;
-		// computer paddle goes off screen to bottom
-		} else if(this.paddle.y > canvas.height) {
-			this.paddle.y = canvas.height - this.height;
+		
+		activePaddle();
+		
+		function activePaddle() {
+			if(!(this.ball.ghostcounter2 === -6)) {
+				renderGhost();
+				moveGhost();
+			} else {
+				moveComputer();
+			}
+		}
+		
+		function renderGhost() {
+			_this.ghost.color = 'rgba(255, 255, 255, .4)';
+		}
+		
+		function moveGhost() {
+			_this.ghost.move(0, diff);
+		}
+		
+		function moveComputer() {
+			_this.ghost.color = 'rgba(255, 255, 255, 0)';
+			_this.paddle.move(0, diff);
 		}
 	};
 	
@@ -259,8 +323,18 @@
 		this.score.count = ball.score2;
 	};
 	
+	// ghost toggle counter for player
+	GhostCounterOne.prototype.update = function(ball) {
+		this.ghostCounter.count = ball.ghostcounter1;	
+	};
+	
+	// ghost toggle countdown for computer
+	GhostCounterTwo.prototype.update = function(ball) {
+		this.ghostCounter.count = ball.ghostcounter2;
+	};
+	
 	// ball position updating / movement
-	Ball.prototype.update = function(paddle1, paddle2, ghost) {
+	Ball.prototype.update = function(paddle1, paddle2, ghost1, ghost2) {
 		var _this = this;
 		this.x += this.x_speed;
 		this.y += this.y_speed;
@@ -272,42 +346,60 @@
 		
 		// verical boundaries
 		if(this.y - this.radius < 0) { // hitting the top wall
-		  this.y = this.radius;
-		  this.y_speed = -this.y_speed;	
+		  bounceBackDown();
 		} else if(this.y + this.radius > height()) { // hitting the bottom wall
-		  this.y = height() - this.radius;
-		  this.y_speed = -this.y_speed;
+		  bounceBackUp();
 		}
 		
 		// hitting the paddles
-		  // if the ghost is active and ball in range of player
-		if((top_x > (canvas.width / 2)) && ghost.color == 'rgba(255, 255, 255, .4)') {
-			if(top_x < (ghost.x + ghost.width) && bottom_x > ghost.x && top_y < (ghost.y + ghost.height) && bottom_y > (ghost.y)) {
+		  // if the ghost is active and ball in range of ghost
+		if(top_x > (canvas.width / 2) && ghost1.color === 'rgba(255, 255, 255, .4)') {
+			if(top_x < (ghost1.x + ghost1.width) && bottom_x > ghost1.x && top_y < (ghost1.y + ghost1.height) && bottom_y > (ghost1.y)) {
 				// hitting ghost paddle right of screen
-				playerPaddleHitBall(ghost);
+				playerPaddleHitBall(ghost1);
+				setTimeout(function() {
+					ghost1.color = 'rgba(255, 255, 255, 0)';
+				}, 150);
+				this.ghostcounter1 = -5;
+				keysToggle[71] = false;
 			}
 		  // if the ghost is not active and ball in range of player
-		} else if(top_x > (canvas.width / 2)) {
+		} else if(top_x > (canvas.width / 2) && ghost1.color !== 'rgba(255, 255, 255, .4)') {
 			if(top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > (paddle1.y)) {
 				// hitting player paddle right of screen
 				playerPaddleHitBall(paddle1);
+				ghostCounterOneCountDown();
 			}
 		  // if ball in range of computer
-		} else {
+		} else if(bottom_x > (canvas.width / 2) && this.x_speed < 0 && ghost2.color === 'rgba(255, 255, 255, .4)') {
+			if(bottom_x > (ghost2.x + ghost2.width) && top_x < (ghost2.x + ghost2.width) && top_y < (ghost2.y + ghost2.height) && bottom_y > (ghost2.y)) {
+				computerPaddleHitBall(ghost2);
+				setTimeout(function() {
+					ghost2.color = 'rgba(255, 255, 255, 0)';
+				}, 150);
+				this.ghostcounter2 = -5;
+			}
+		} else if(bottom_x < (canvas.width / 2) && ghost2.color !== 'rgba(255, 255, 255, .4)') {
 			if(bottom_x > (paddle2.x + paddle2.width) && top_x < (paddle2.x + paddle2.width) && top_y < (paddle2.y + paddle2.height) && bottom_y > (paddle2.y)) {
 				// hitting computer paddle left of screen
-				computerPaddleHitBall();
+				computerPaddleHitBall(paddle2);
+				ghostCounterTwoCountDown();
 			}
 		}
+
 		
 		// reset and scoring
-		if(this.x < 0 || this. x > width()) { // point was scored
+		if(this.x < 0 || this. x > width()) { // point was scored	
 		  if(this.x < 0) {
 			  // player scores
 			  this.score1 += 1;
+			  // player ghost counter is reset
+			  this.ghostcounter1 = -5;
 		  } else if(this.x > canvas.width) {
 			  //computer scores
 			  this.score2 += 1;
+			  // computer ghost counter is reset
+			  this.ghostcounter2 = -5;
 		  }
 		  // reset - randomize starting ball direction
 		  this.x_speed = Math.random() >= .5 ? .5*this.radius : -.5*this.radius;
@@ -315,6 +407,18 @@
 		  this.y_speed = 0;
 		  this.x = canvas.width/2 - .01*canvas.width;
 		  this.y = canvas.height/2 - .01*canvas.width;
+		}
+		
+		// ball bounce off top wall
+		function bounceBackDown() {
+			_this.y = _this.radius;
+		  	_this.y_speed = -_this.y_speed;	
+		}
+		
+		// ball bounce off bottom wall
+		function bounceBackUp() {
+		  _this.y = this.height() - _this.radius;
+		  _this.y_speed = -_this.y_speed;
 		}
 		
 		// ball movement after hitting player paddles
@@ -325,10 +429,28 @@
 		}
 		
 		// ball movement after hitting computer paddle
-		function computerPaddleHitBall() {
+		function computerPaddleHitBall(paddle) {
 			_this.x_speed = _this.radius;
-			_this.y_speed += (paddle2.y_speed/6);
+			_this.y_speed += (paddle.y_speed/6);
 			_this.x += _this.x_speed;
+		}
+		
+		// ghost counter countdown for the player after ball is hit
+		function ghostCounterOneCountDown() {
+			if(_this.ghostcounter1 === -1) {
+				_this.ghostcounter1 = 'Press "g"';
+			} else if(_this.ghostcounter1 < 0) {
+				_this.ghostcounter1 += 1;
+			} 
+		}
+		
+		// ghost counter countdown for the computer after ball is hit
+		function ghostCounterTwoCountDown() {
+			if(_this.ghostcounter2 === -1) {
+				_this.ghostcounter2 = 'AI - random';
+			} else if(_this.ghostcounter2 < 0) {
+				_this.ghostcounter2 += 1;
+			} 
 		}
 	};
 
@@ -339,8 +461,10 @@
 	// game asset calls
 	var player = new Player();
 	var computer = new Computer();
-	var scoreone = new ScoreOne();
-	var scoretwo = new ScoreTwo();
+	var score1 = new ScoreOne();
+	var score2 = new ScoreTwo();
+	var ghostcounter1 = new GhostCounterOne();
+	var ghostcounter2 = new GhostCounterTwo();
 	var ball = new Ball((canvas.width/2 - .01*canvas.width), (canvas.height/2 - .01*canvas.width));
 	
 	// render the board and the game assets
@@ -350,8 +474,10 @@
 		player.render();
 		computer.render();
 		ball.render();
-		scoreone.render();
-		scoretwo.render();
+		score1.render();
+		score2.render();
+		ghostcounter1.render();
+		ghostcounter2.render();
 	};
 
 // END - Initialization
